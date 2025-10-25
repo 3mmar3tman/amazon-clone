a3012dce567791268a7f2eec178e67b6c3fd7a50
Ammar Yasser <3mmar3tman@example.com>
2025-10-25 17:48:39 +0300

    feat: add Home and Checkout pages, and complete Login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from "react";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+import { React, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/Login";
-
+import { auth } from "./firebase";
+import { useAuth } from "./context/GlobalState";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
 function App() {
+  const { dispatch } = useAuth();
+  //   const stripePromise = loadStripe(
+  //   "pk_test_51M8qBGGgMfG9lrQtwQD96xCOGZUGCke60ampxG6veTlOT3bRrKATseNAiOzXTUNYB7GyXIDHzsgSTqkcMXgnHCXL00jKa12Jue"
+  // );
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]);
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/Checkout"
+          element={
+            <>
+              <Header />
+              <Checkout />
+            </>
+          }
+        />
         <Route path="/Login" element={<Login />} />
         <Route path="*" element={<h1>page not found</h1>} />
       </Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,43 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../images/login-logo.png";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../firebase";
 import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/GlobalState";
 function Login() {
+  const user = useAuth();
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const navigate = useNavigate();
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password).then((auth) => {
+      if (auth) {
+        navigate("/");
+      }
+    });
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        if (auth) {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  console.log(user);
+
   return (
     <div className="login">
       <Link to="/">
@@ -12,17 +47,25 @@ function Login() {
         <h1>signIn</h1>
         <form>
           <h5>Email</h5>
-          <input type="email" value="" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
           <h5>password</h5>
-          <input type="password" value="" />
-          <button type="submit " className="login-signInBtn">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+          />
+          <button type="submit " className="login-signInBtn" onClick={signIn}>
             sign in
           </button>
           <p>
             By continuing, you agree to Amazon's Fake Clone Conditions of Use
             and Privacy Notice.
           </p>
-          <button className="login-registerBtn">
+          <button className="login-registerBtn" onClick={register}>
             Create your Amazon Account
           </button>
         </form>
